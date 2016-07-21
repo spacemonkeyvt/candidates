9f64ccacea6247f4f1b46ec506744c8ad0fd644e
Vishal Tiwari <vishal.tiwari.09@gmail.com>
2016-07-21 14:39:30 -0400

    Method 'qualified_candidates' struggling to work. Task 4.

--- filters.rb
+++ filters.rb
@@ -1,8 +1,28 @@
 def find(id)
-  @candidates.detect {|x| x[:id] == id ? x : nil}
+  @candidates.detect {|person| person[:id] == id ? person : nil}
 end
 
 def experienced?(candidate)
   yes_to_exp = candidate[:years_of_experience] >= 2
   return "Candidate has the requested years of experience: #{yes_to_exp}"
 end
+
+def qualified_candidates
+    @candidates.each do |person|
+      return "#{person} is qualified!\n\n" if (person[:years_of_experience] >= 2) && (person[:github_points] >= 100) && (person[:languages] == ("Ruby" || "Python")) && ([:date_applied] >= 15.days.ago.to_date)) && ([:age] >= 18)
+    end
+end
+
+# def qualified_candidates
+#   #takes in all candidates
+#   #returns candidates who
+#     ## experienced? == true
+#     ## [:github_points] >= 100
+#     ## [:languages] == ("Ruby" || "Python")
+#     ## [:date_applied] >= 15.days.ago.to_date)
+#     ## [:age] >= 18
+
+#     @candidates.each do |person|
+#       return "#{person} is qualified!\n\n" if person[:years_of_experience] >= 2 && person[:github_points] >= 100 && person[:languages] == ("Ruby" || "Python") && [:date_applied] >= 15.days.ago.to_date) && [:age] >= 18
+#     end
+# end
